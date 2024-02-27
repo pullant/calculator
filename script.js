780323a8c9f07c4a90c387084ee1fe8ab8710a76
pullant <108009450+pullant@users.noreply.github.com>
2024-02-27 16:07:00 -0300

    20240227160700

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-//TODO
-// -	-3 + 1 breaks
-// -	3.5 + 1 breaks no dot operator
-// -	3.5.6.7
-// -	some / 0 capture error
-let displayValue = "";
-let displayHistory = [];
-let post = "";
-let prev = "";
-let prevLog = [];
-const main	=	document.querySelector("main");
-const display	=	document.querySelector("#display");
-const history	=	document.querySelector("#history");
+let displayValue	= "";
+let displayHistory	= [];
+let post		= "";
+let prev		= "";
+let prevLog		= [];
+const main		= document.querySelector("main");
+const display		= document.querySelector("#display");
+const history		= document.querySelector("#history");
 
 main.addEventListener("click", (e) => {
 	switch (e.target.id) {
@@ -98,15 +93,107 @@ main.addEventListener("click", (e) => {
 		case "equalsA":
 		case "equalsB":
 			console.log("=");
-			displayHistory.push(displayValue); // add operation to history
-			displayValue = resolveOperation(displayValue); // result of operation
+			displayHistory.push(displayValue);
+			displayValue = resolveOperation(displayValue);
 			history.innerText = buildHistory(displayValue);
 			display.innerText = displayValue;
 			break;
 	}
+	display.scrollLeft = display.scrollWidth;
 });
 
-main.addEventListener("click", (e) => {
+document.addEventListener("keydown", (e) => {
+	switch (e.key) {
+		case "1":
+			console.log("1");
+			displayValue += "1";
+			display.innerText = displayValue;
+			break;
+		case "2":
+			console.log("2");
+			displayValue += "2";
+			display.innerText = displayValue;
+			break;
+		case "3":
+			console.log("3");
+			displayValue += "3";
+			display.innerText = displayValue;
+			break;
+		case "4":
+			console.log("4");
+			displayValue += "4";
+			display.innerText = displayValue;
+			break;
+		case "5":
+			console.log("5");
+			displayValue += "5";
+			display.innerText = displayValue;
+			break;
+		case "6":
+			console.log("6");
+			displayValue += "6";
+			display.innerText = displayValue;
+			break;
+		case "7":
+			console.log("7");
+			displayValue += "7";
+			display.innerText = displayValue;
+			break;
+		case "8":
+			console.log("8");
+			displayValue += "8";
+			display.innerText = displayValue;
+			break;
+		case "9":
+			console.log("9");
+			displayValue += "9";
+			display.innerText = displayValue;
+			break;
+		case "0":
+			console.log("0");
+			displayValue += "0";
+			display.innerText = displayValue;
+			break;
+		case ".":
+			console.log(".");
+			displayValue += ".";
+			display.innerText = displayValue;
+			break;
+		case "+":
+			console.log("+");
+			displayValue += " + ";
+			display.innerText = displayValue;
+			break;
+		case "/":
+			e.preventDefault();
+			console.log("/");
+			displayValue += " / ";
+			display.innerText = displayValue;
+			break;
+		case "*":
+			console.log("*");
+			displayValue += " * ";
+			display.innerText = displayValue;
+			break;
+		case "-":
+			console.log("-");
+			displayValue += " - ";
+			display.innerText = displayValue;
+			break;
+		case "Backspace":
+			e.preventDefault();
+			console.log("⌫");
+			displayValue = display.textContent.slice(0, -1);
+			display.innerText = displayValue;
+			break;
+		case "Enter":
+			console.log("=");
+			displayHistory.push(displayValue);
+			displayValue = resolveOperation(displayValue);
+			history.innerText = buildHistory(displayValue);
+			display.innerText = displayValue;
+			break;
+	}
 	display.scrollLeft = display.scrollWidth;
 });
 
@@ -151,13 +238,13 @@ function resolveOperation(display) {
 			parsedOperation = parseOperation(display);
 			prevLog[i] = prev;
 			if (i > 0 && prevLog[i] === prevLog[i - 1]) {
-				console.log(`-------- ERROR: Unsupported operation: op2 NO CHANGE ----------------`);
+				console.log(`-------- ERROR: Unsupported operation: op2 NO CHANGE ---------------`);
 				return "ERROR"
 			}
 			display = calculate(parsedOperation) + displayValue;
 		}
 	}
-	console.log(`-------- ERROR: Unsupported operation: op1 OVER MAX OPERATIONS ------`);
+	console.log(`-------- ERROR: Unsupported operation: op1 OVER MAX OPERATIONS -----`);
 	return "ERROR"
 }
 
@@ -169,7 +256,7 @@ function parseOperation(operation) {
 	operation = operation.replace(/\s/g, "")
 	console.log("Parsing:\n\tprev: ", operation);
 	prev = operation;
-	let numbers = /-?\d+(\.\d+)?/g;
+	let numbers = /[-\+]?\d+(\.\d+)?/g;
 	// make all - into (* -1)
 	//operation = operation.replace(/-(\d+(\.\d+)?)/g, (match, group) => "+" + group + "*-1");
 	numbers = operation.match(numbers);
@@ -182,14 +269,22 @@ function parseOperation(operation) {
 		console.log(operation);
 	operation = operation.replace(b, "n");
 		console.log(operation);
-	let operationLength = operation.match(/.*n.*n/g)[0].length
-		console.log(operationLength);
+
+	if (!b) {
+		displayValue = "";
+		return [+a];
+	}
+	else {
+		let operationLength = operation.match(/.*n.*n/g)[0].length
+			console.log(operationLength);
+	}
 	if ( operationLength === 2) {
 		operation = operation.replaceAll("n", "");
 		operator = "+";
 	}
 	else if ( operationLength > 3) {
 		displayValue = "";
+		console.log(`-------- ERROR: Unsupported operator: TOO MANY OPERATORS [ ${operation} ] --`);
 		return "ERROR";
 	}
 	else {
@@ -218,10 +313,6 @@ function parseOperation(operation) {
 	return [+a, +b, operator];
 }
 
-function isNumber(character) {
-	return !isNaN(character) ;
-}
-
 /**
  * @param {number} a First operand.
  * @param {number} b Second operand.
@@ -229,22 +320,39 @@ function isNumber(character) {
  * @returns {number} Result.
  */
 function calculate([a, b, operator]) {
-	if (!operator) {
-		return a;
-	}
+	console.log("a:", a, "b:", b, "operator:", operator);
 
+	if (!operator)
+		return a;
+
+	if (a === "E")
+		return "ERROR";
+
+	let result;
 	switch (operator) {
 		case "+":
-			return add(a, b);
+			result = add(a, b);
+			break;
 		case "-":
-			return subtract(a, b);
+			result = subtract(a, b);
+			break;
 		case "*":
-			return multiply(a, b);
+			result = multiply(a, b);
+			break;
 		case "/":
-			return divide(a, b);
+			result = divide(a, b);
+			break;
 		default:
-			console.log(`-------- ERROR: Unsupported operation: op4 UNSUPPORTED OPERATOR ------`);
+			console.log(`-------- ERROR: Unsupported operation: op3 UNSUPPORTED OPERATOR [ ${operator} ] -`);
 			return "ERROR";
+	}
+
+	if (result === Infinity || result === -Infinity) {
+		console.log(`-------- ERROR: Unsupported result: op4 [ Infinity or -Infinity ] --`);
+		return "ERROR";
+	}
+	else {
+		return result;
 	}
 }
 
